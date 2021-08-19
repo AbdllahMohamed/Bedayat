ba5099cf61f2244c0c439389c1e6df51b6ad81b1
AbdllahMohamed <devAbdo2000@gmail.com>
2021-08-19 05:08:45 +0200

    commit

---
+++ lib/app_images/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const appLogo = 'assets/images/logo.png';
+  static const appEmailIcon = 'assets/images/email.png';
+  static const appPasswordIcon = 'assets/images/password.png';
+  static const appPhoneIcon = 'assets/images/phone.png';
+}
