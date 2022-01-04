95ad8de69a6c1a2e4a6dda381c967952495fe8bd
Hossam Hani <hossamhani.t@gmail.com>
2022-01-04 23:18:07 +0200

    child status logic

---
+++ lib/UI/screens/report/report_editor/report_editor_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+class ReportEditorController extends GetxController {
+  RxDouble temp = 37.00.obs;
+  RxDouble sleep = 0.00.obs;
+
+  RxString childMood = "high".obs;
+  changeChildMood(newChildMood) => childMood.value = newChildMood;
+
+  RxString foodStatus = "high".obs;
+  changeFoodStatus(newFoodStatus) => foodStatus.value = newFoodStatus;
+
+  RxString drinkStatus = "high".obs;
+  changeDrinkStatus(newDrinkStatus) => drinkStatus.value = newDrinkStatus;
+
+  RxString sleepStatus = "high".obs;
+  changeSleepStatus(newSleepStatus) => sleepStatus.value = newSleepStatus;
+}
